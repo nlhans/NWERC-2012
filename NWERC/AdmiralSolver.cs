633e70caa8c9735794469633e21893ebcf7118ff
Hans de Jong <hans1990@gmail.com>
2012-11-29 22:17:34 +0100

    Solutions for A & B

---
+++ NWERC/AdmiralSolver.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NWERC.Admiral
+{
+    public class Waypoint
+    {
+        public int ID;
+        public List<Passage> Routes;
+
+        public Waypoint(int id)
+        {
+            ID = id;
+            Routes = new List<Passage>();
+        }
+    }
+    
+    public class RoutePair
+    {
+        public List<Route> Red;
+        public List<Route> Blue;
+
+        public RoutePair(List<Route> red, List<Route> blue)
+        {
+            Red = red;
+            Blue = blue;
+        }
+    }
+
+    public class Route
+    {
+        public Waypoint Start;
+        public Passage Passage;
+
+        public Route(Waypoint start, Passage passage)
+        {
+            Start = start;
+            Passage = passage;
+        }
+    }
+
+    public class Passage
+    {
+        public Waypoint Target;
+        public int Cost;
+
+        public Passage(Waypoint target, int cost)
+        {
+            Target = target;
+            Cost = cost;
+        }
+    }
+
+    public class AdmiralSolver
+    {
+        private readonly IEnumerable<Waypoint> _map;
+        private List<Waypoint> _closedNodes = new List<Waypoint>();
+
+        public AdmiralSolver(IEnumerable<Waypoint> map)
+        {
+            _map = map;
+        }
+
+        private static Waypoint GetOrCreate(int id, SortedList<int, Waypoint> waypoints)
+        {
+            Waypoint waypoint;
+
+            if (!waypoints.ContainsKey(id))
+            {
+                waypoint = new Waypoint(id);
+                waypoints.Add(id, waypoint);
+            }
+            else
+            {
+                waypoint = waypoints[id];
+            }
+
+            return waypoint;
+        }
+
+        public static IEnumerable<Waypoint> CreateMapFromString(IEnumerable<string> entries)
+        {
+            var waypoints = new SortedList<int, Waypoint>();
+
+            foreach (string entryPassage in entries)
+            {
+                var data = entryPassage.Split(' ');
+                var startPosition = int.Parse(data[0]);
+                var endPosition = int.Parse(data[1]);
+                var cost = int.Parse(data[2]);
+
+                var startWaypoint = GetOrCreate(startPosition, waypoints);
+                var destWaypoint = GetOrCreate(endPosition, waypoints);
+                
+                startWaypoint.Routes.Add(new Passage(destWaypoint, cost));
+            }
+
+            return waypoints.Select(x => x.Value);
+        }
+
+
+
+        public RoutePair FindRoute(int targetId)
+        {
+            var startWaypoint = _map.First();
+            var route = new List<Route>();
+
+            var  solutions = Visit(targetId, startWaypoint, route);
+            //IOrderedEnumerable<List<Route>> best_routes =solutions.OrderBy(x => x.Sum(c => c.Passage.Cost));
+
+            var routepairs = new List<RoutePair>();
+
+            foreach(var route1 in solutions)
+            {
+                foreach(var route2 in solutions)
+                {
+                    if (route2 == route1) continue;
+
+                    var waypoints1 = route1
+                        .Where(x => x.Passage.Target.ID != targetId)
+                        .Select(r => r.Passage.Target.ID);
+
+                    var waypoints2 = route2
+                        .Where(x => x.Passage.Target.ID != targetId)
+                        .Select(r => r.Passage.Target.ID);
+
+                    if(waypoints1.Any(waypoints2.Contains))
+                    {
+                        continue;
+                    }
+
+                    if(!routepairs.Any(x => x.Red == route2 && x.Blue == route1))
+                    {
+                        routepairs.Add(new RoutePair(route1, route2));    
+                    }
+
+                    
+                }
+            }
+
+            return routepairs
+                .OrderBy(x => x.Red.Sum(c =>c.Passage.Cost) + x.Blue.Sum(c => c.Passage.Cost))
+                .FirstOrDefault();
+        }
+
+        public List<List<Route>> Visit(int targetID, Waypoint waypoint, List<Route> route)
+        {
+            List<List<Route>> routes = new List<List<Route>>();
+            
+            foreach(Passage passage in waypoint.Routes)
+            {
+                if (route.Any(x => x.Start == passage.Target))
+                {
+                    continue;
+                }
+
+                List<Route> followedRoute = new List<Route>(route);
+                followedRoute.Add(new Route(waypoint, passage));
+                if(passage.Target.ID == targetID)
+                {
+                    routes.Add(followedRoute);
+                }
+                else
+                {
+                    routes.AddRange(Visit(targetID, passage.Target, followedRoute));
+                }
+            }
+
+            return routes;
+        }
+
+    }
+
+    [TestFixture]
+    public class AdmiralSolverTest
+    {
+        [Test]
+        public void CreateMap()
+        {
+            IEnumerable<Waypoint> map = BuildMap();
+            Assert.AreEqual(6, map.Count());
+
+            var firstWayPoint = map.FirstOrDefault(x => x.ID == 1);
+
+            Assert.NotNull(firstWayPoint);
+            Assert.AreEqual(3, firstWayPoint.Routes.Count);
+
+        }
+
+        private IEnumerable<Waypoint> BuildMap()
+        {
+            var entries = new List<string>();
+
+            entries.Add("1 2 23");
+            entries.Add("1 3 12");
+            entries.Add("1 4 99");
+            entries.Add("2 5 17");
+            entries.Add("2 6 73");
+            entries.Add("3 5 3");
+            entries.Add("3 6 21");
+            entries.Add("4 6 8");
+            entries.Add("5 2 33");
+            entries.Add("5 4 5");
+            entries.Add("6 5 20");
+
+            return AdmiralSolver.CreateMapFromString(entries);
+        }
+
+
+        [Test]
+        public void TestVisit()
+        {
+            var map = BuildMap();
+            var solver = new AdmiralSolver(map);
+            var route = solver.FindRoute(6);
+
+            Assert.AreEqual(86, route.Blue.Sum(x => x.Passage.Cost) + route.Red.Sum(x => x.Passage.Cost));
+        }
+    }
+}
---
+++ NWERC/BeerPressure.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NWERC
+{
+    public class Pub : ICloneable
+    {
+        public int Votes;
+        public int Id;
+        public double Chance;
+
+        public Pub(int votes, int id, double chance)
+        {
+            Votes = votes;
+            Id = id;
+            Chance = chance;
+        }
+
+        public object Clone()
+        {
+            return new Pub(Votes, Id, Chance);
+
+        }
+    }
+
+    public class Poll
+    {
+        public int Contenders;
+
+        public List<Pub> Pubs = new List<Pub>();
+    }
+
+    public class BeerPressure
+    {
+        public static Poll ParseLines(string pubs, string votes)
+        {
+            Poll poll = new Poll();
+
+            string[] pubs_split = pubs.Split(' ');
+            string[] votes_split = votes.Split(' ');
+            poll.Contenders = int.Parse(pubs_split[1]);
+
+            for (int i = 0; i < int.Parse(pubs_split[0]); i++)
+            {
+                poll.Pubs.Add(new Pub(int.Parse(votes_split[i]), i + 1, 0.0));
+            }
+            return poll;
+        }
+
+        public static Poll DeterminePollResult(Poll poll)
+        {
+            var result = VoteStudent(poll);
+            var total_permutations =  result.Sum(x => x.Value);
+            poll.Pubs.ForEach(x => x.Chance = result[x.Id] / total_permutations);
+
+            return poll;
+        }
+
+        public static Dictionary<int, double> VoteStudent(Poll poll)
+        {
+            Dictionary<int, double> total_result = new Dictionary<int, double>();
+            poll.Pubs.ForEach(x => total_result.Add(x.Id, 0.0));
+
+            int students = poll.Pubs.Sum(x => x.Votes);
+
+            if (students == poll.Contenders)
+            {
+                // All votes are casted
+                var maximumVotes = poll.Pubs.Max(x => x.Votes);
+                var pubsWithMaximumVotes = poll.Pubs.Count(x => x.Votes == maximumVotes);
+                foreach (Pub p in poll.Pubs.Where(x => x.Votes == maximumVotes))
+                    total_result[p.Id] += 1.0/pubsWithMaximumVotes;
+
+
+            }
+            else
+            {
+                // Vote for this student with all permutations
+                foreach (Pub pub in poll.Pubs)
+                {
+                    for (int i = 0; i < pub.Votes; i++)
+                    {
+                        Poll p = new Poll();
+                        p.Contenders = poll.Contenders;
+                        p.Pubs = poll.Pubs.Select(x => x.Clone()).Cast<Pub>().ToList();
+                        p.Pubs.FirstOrDefault(x => x.Id == pub.Id).Votes++;
+
+                        var result = VoteStudent(p);
+
+                        foreach (var r in result)
+                        {
+                            total_result[r.Key] += r.Value;
+                        }
+                    }
+
+                }
+            }
+
+            return total_result;
+        }
+
+    }
+    
+    public class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _comprarer;
+
+        public LambdaEqualityComparer(Func<T, T, bool> comprarer)
+        {
+            _comprarer = comprarer;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _comprarer(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return 0;
+        }
+    }
+
+    [TestFixture]
+    public class BeerPressureTest
+    {
+        [Test]
+        public void ParsePubs()
+        {
+            Poll poll = ParsePoll();
+
+            Assert.AreEqual(3, poll.Pubs.Count);
+            Assert.AreEqual(5, poll.Pubs.Sum(x => x.Votes));
+            Assert.AreEqual(7, poll.Contenders);
+
+        }
+
+        private Poll ParsePoll()
+        {
+            string pubs = "3 7";
+            string votes = "3 1 1";
+
+            return BeerPressure.ParseLines(pubs,votes);
+        }
+
+
+        [Test]
+        public void RunPoll()
+        {
+            Poll poll = ParsePoll();
+
+            Poll result = BeerPressure.DeterminePollResult(poll);
+
+            Assert.AreEqual(28.0 / 30, result.Pubs.FirstOrDefault(x => x.Id == 1).Chance);
+            Assert.AreEqual(1.0 / 30, result.Pubs.FirstOrDefault(x => x.Id == 2).Chance);
+            Assert.AreEqual(1.0 / 30, result.Pubs.FirstOrDefault(x => x.Id == 3).Chance);
+
+        }
+
+    }
+}
